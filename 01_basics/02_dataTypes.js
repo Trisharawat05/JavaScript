61118f460c4c67f358aee97335fab7eccf822721
Trisharawat05 <148178489+Trisharawat05@users.noreply.github.com>
2025-05-21 06:43:37 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+ //code readibility should be high
+
+//original documentation for javascript standards is tc39 Ecma
+//use mdn
+
+let name = "Trisha"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

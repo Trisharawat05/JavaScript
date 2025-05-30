2a0842bfc75954ebc5612ff18a849b46e79188f8
Trisharawat05 <148178489+Trisharawat05@users.noreply.github.com>
2025-05-30 19:39:53 +0000

    Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,41 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "mar"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+
+    default:
+        console.log("default case");
+        break;
+}
